df30ae0c2f89a5a5ddbae82914e4f161ad061c5f
daniel <danieldubonxd@gmail.com>
2023-03-21 15:52:49 -0600

    cva

--- src/app/components/ui/Paragraph.tsx
+++ src/app/components/ui/Paragraph.tsx
@@ -1,9 +1,14 @@
 import { FC } from 'react'
+import { cva } from 'class-variance-authority'
 
 interface IParagraphProps {
 	title: string
 }
 
+const Pvariants = cva('max-w-prose', {
+	variants: { sizes: { default: 'text-base', sm: 'text-sm' } },
+})
+console.log(Pvariants)
 const Paragraph: FC<IParagraphProps> = ({ title }) => {
 	return <div>{title}</div>
 }
