80c41e19de16e337abaaf3c32b05baa006ad86df
daniel <danieldubonxd@gmail.com>
2023-04-14 08:47:53 -0600

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Paragraph from '@/components/ui/Paragraph'
 import '@/styles/index.css'
 
 export default function Home() {
-	return (
-		<main>
-			<Paragraph size="sm">hi from daniel page</Paragraph>
-		</main>
-	)
+	return <main></main>
 }
