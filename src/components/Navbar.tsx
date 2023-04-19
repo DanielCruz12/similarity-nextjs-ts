fa2ad8c10e27a5ebae034fb8a10349f418d2f528
daniel <danieldubonxd@gmail.com>
2023-04-18 22:24:16 -0600

    all packages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,7 @@ import { buttonVariants } from './ui/Button'
 
 const Navbar = () => {
 	const session = 1
-	/* 	const hola = 1
-	 */ return (
+	return (
 		<div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
 			<div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
 				<Link href="/" className={buttonVariants({ variant: 'link' })}>
