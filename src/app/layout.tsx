1ba26444288af204f9abe156ab87402a9e76e9c1
daniel <danieldubonxd@gmail.com>
2023-05-11 21:36:19 -0600

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 			<body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
 				<Providers>
 					{children}
-					<Toaster position="bottom-right"/>
+					<Toaster position="bottom-right" />
 					<Navbar />
 				</Providers>
 				{/* Allow more height for mobile menu on mobile */}
--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,23 +1,22 @@
 import { toast } from './ui/toast'
 
-
 const SignInButton = () => {
-/* 	const [isLoading, setIsLoading] = useState<boolean>(false)
- */
+	/* 	const [isLoading, setIsLoading] = useState<boolean>(false)
+	 */
 	const signInWithGoogle = async () => {
 		try {
 			toast({
-			  title: 'Error signing in',
-			  message: 'Please try again later.',
-			  type: 'error',
+				title: 'Error signing in',
+				message: 'Please try again later.',
+				type: 'error',
 			})
-		  /* await signIn('google') */
+			/* await signIn('google') */
 		} catch (error) {
 			console.log(error)
 		}
-	  }
-	
-	return <div onClick={signInWithGoogle} >sign In</div>
+	}
+
+	return <div onClick={signInWithGoogle}>sign In</div>
 }
 
 export default SignInButton
